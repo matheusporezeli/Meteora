49703c1e10901205458768e51c2e72bb50063e7a
Matheus Porezeli <112051389+matheusporezeli@users.noreply.github.com>
2023-07-11 11:29:39 -0300

    --lista de produtos via json

---
+++ js/cardProdutos.js
@@ -0,0 +1,16 @@
+let listaProdutos = document.querySelector("#lista_produtos");
+
+fetch("./produtos.json").then((response) => {
+  response.json().then((element) => {
+    element.produtos.map((produto) => {
+      listaProdutos.innerHTML += `
+                <li class="produtos_card">
+                    <img src="${produto.imagem}" class="produtos__imagem">
+                    <h2 class="produtos__titulo">${produto.titulo}</h2>
+                    <p class="produtos__descricao">${produto.descricao}</p>
+                    <h2 class="produtos__preco">${produto.preco}</h2>
+                    <button>Ver mais</button>
+                </li>`;
+    });
+  });
+});
